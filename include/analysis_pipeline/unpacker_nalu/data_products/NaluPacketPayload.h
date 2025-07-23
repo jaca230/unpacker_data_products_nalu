085a821f55d6507597eb55b41327b04d03c4e755
jaca230 <jaca230@uky.edu>
2025-07-22 21:28:47 -0400

    attempt to use setter to set trace from buffer

--- include/analysis_pipeline/unpacker_nalu/data_products/NaluPacketPayload.h
+++ include/analysis_pipeline/unpacker_nalu/data_products/NaluPacketPayload.h
@@ -12,13 +12,21 @@ public:
     NaluPacketPayload();
     ~NaluPacketPayload();
 
-    std::array<uint16_t, 32> trace;
+    std::array<uint16_t, 32> trace;  // *OPTIONS={GetMethod="GetTrace"; SetMethod="SetTrace"}
+
+    const uint16_t* GetTrace() const { return trace.data(); }
+
+    void SetTrace(const uint8_t* buffer, size_t offset) {
+        // Copy from buffer+offset into trace array
+        std::memcpy(trace.data(), buffer + offset, trace.size() * sizeof(uint16_t));
+    }
 
     std::string String() const;
     void Show() const override;
 
     ClassDefOverride(NaluPacketPayload, 1)
 };
+
 
 } // namespace dataProducts
 
