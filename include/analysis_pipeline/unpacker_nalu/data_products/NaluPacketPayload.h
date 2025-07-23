eface6699eb9ed5314e4d9540a65f5a8518ff5b2
jaca230 <jaca230@uky.edu>
2025-07-22 21:45:26 -0400

    removed setter for payload

--- include/analysis_pipeline/unpacker_nalu/data_products/NaluPacketPayload.h
+++ include/analysis_pipeline/unpacker_nalu/data_products/NaluPacketPayload.h
@@ -12,21 +12,13 @@ public:
     NaluPacketPayload();
     ~NaluPacketPayload();
 
-    std::array<uint16_t, 32> trace;  // *OPTIONS={GetMethod="GetTrace"; SetMethod="SetTrace"}
-
-    const uint16_t* GetTrace() const { return trace.data(); }
-
-    void SetTrace(const uint8_t* buffer, size_t offset) {
-        // Copy from buffer+offset into trace array
-        std::memcpy(trace.data(), buffer + offset, trace.size() * sizeof(uint16_t));
-    }
+    std::array<uint16_t, 32> trace;
 
     std::string String() const;
     void Show() const override;
 
     ClassDefOverride(NaluPacketPayload, 1)
 };
-
 
 } // namespace dataProducts
 
